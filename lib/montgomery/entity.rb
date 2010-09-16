ff681ad0ebbac959e21a19b754d9c81f7bc88947
Wojciech Piekutowski <wojciech@piekutowski.net>
2010-09-16 23:59:24 +0200

    Avoid warnings about not initialized @_id

--- lib/montgomery/entity.rb
+++ lib/montgomery/entity.rb
@@ -2,6 +2,9 @@ module Montgomery::Entity
   attr_reader :_id
 
   def initialize(doc={})
+    # prevent warnings about unset @_id
+    instance_variable_set(:@_id, nil) unless doc.has_key?('_id')
+
     doc.each do |key, value|
       instance_variable_set("@#{key}", value)
     end
@@ -15,6 +18,8 @@ module Montgomery::Entity
       doc[key] = value
     end
     doc['_class'] = self.class.to_s
+    # _id == nil will cause MongoDB to insert without setting ObjectId
+    doc.delete('_id') unless doc['_id']
     doc
   end
 end
