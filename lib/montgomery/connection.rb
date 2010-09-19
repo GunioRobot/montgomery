4a8e767003cba71e8382a1dbf5adffe6981aa53b
Wojciech Piekutowski <wojciech@piekutowski.net>
2010-09-19 17:40:57 +0200

    Connection: delegate common methods to Mongo::Connection

--- lib/montgomery/connection.rb
+++ lib/montgomery/connection.rb
@@ -1,4 +1,19 @@
 class Montgomery::Connection
+  include Montgomery::Delegator
+
+  # properties
+  delegate :arbiters, :auths, :checked_out, :host, :logger,
+      :nodes, :port, :primary, :secondaries, :size, :sockets,
+      to: :mongo_connection
+
+  # methods
+  delegate :add_auth, :apply_saved_authentication, :clear_auths,
+      :close, :connect, :connect_to_master, :connected?, :copy_database,
+      :database_info, :database_names, :drop_database, :format_pair,
+      :get_request_id, :pair_val_to_connection, :parse_uri, :receive_message,
+      :remove_auth, :send_message, :send_message_with_safe_check, :server_info,
+      :server_version, :slave_ok?, to: :mongo_connection
+
   def initialize(host=nil, port=nil, options={})
     @mongo_connection = Mongo::Connection.new(host, port, options)
   end
--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -19,5 +19,24 @@ describe 'Montgomery::Connection' do
     it 'should raise exception when calling #db' do
       lambda { @connection.db('montgomery') }.should.raise(RuntimeError)
     end
+
+    delegated_properties = [:arbiters, :auths, :checked_out, :host, :logger,
+      :nodes, :port, :primary, :secondaries, :size, :sockets]
+    delegated_methods = [:add_auth, :apply_saved_authentication, :clear_auths,
+      :close, :connect, :connect_to_master, :connected?, :copy_database,
+      :database_info, :database_names, :drop_database, :format_pair,
+      :get_request_id, :pair_val_to_connection, :parse_uri, :receive_message,
+      :remove_auth, :send_message, :send_message_with_safe_check, :server_info,
+      :server_version, :slave_ok?]
+
+    (delegated_properties + delegated_methods).each do |message|
+      it "should delegate #{message} to Mongo::Collection" do
+        Mongo::Connection.any_instance.expects(message)
+        @connection.send(message)
+
+        Mongo::Connection.any_instance.expects(message).with(:args)
+        @connection.send(message, :args)
+      end
+    end
   end
 end
