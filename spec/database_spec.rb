ead6e712455065a3a2bee3361d7cf09d01e4e105
Wojciech Piekutowski <wojciech@piekutowski.net>
2010-09-19 22:22:52 +0200

    Database spec runs without MongoDB

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -2,18 +2,19 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'spec_helper')
 
 describe 'Montgomery::Database' do
   before do
-    mongo_connection = Mongo::Connection.new
-    mongo_database = mongo_connection.db('montgomery')
-    @database = Montgomery::Database.new(mongo_database)
+    @mongo_database = mock('Mongo::Database')
+    @database = Montgomery::Database.new(@mongo_database)
   end
 
   it 'should return a collection' do
+    @mongo_database.expects(:collection).with('items').returns(stub(name: 'items'))
+
     collection = @database.collection('items')
     collection.should.be.an.instance_of(Montgomery::Collection)
     collection.name.should.equal('items')
   end
 
   it 'should return a Mongo::Database' do
-    @database.to_mongo.should.be.instance_of Mongo::DB
+    @database.to_mongo.should.equal @mongo_database
   end
 end
