bd2a9cfc0433bb53f88d16981cd9337ba2ce969e
Wojciech Piekutowski <wojciech@piekutowski.net>
2011-01-21 23:54:29 +0100

    Update Entity specs to RSpec

--- spec/entity_attribute_spec.rb
+++ spec/entity_attribute_spec.rb
@@ -7,14 +7,14 @@ end
 describe 'Montgomery::Entity' do
   it 'should define attr_reader with montgomery_attr_reader' do
     AttributeItem.montgomery_attr_reader :size
-    AttributeItem.public_instance_methods.should.include(:size)
-    AttributeItem.montgomery_attrs.should.include(:size)
+    AttributeItem.public_instance_methods.should include(:size)
+    AttributeItem.montgomery_attrs.should include(:size)
   end
 
   it 'should define attr_accessor with montgomery_attr_accessor' do
     AttributeItem.montgomery_attr_accessor :weight
-    AttributeItem.public_instance_methods.should.include(:weight)
-    AttributeItem.public_instance_methods.should.include(:weight=)
-    AttributeItem.montgomery_attrs.should.include(:weight)
+    AttributeItem.public_instance_methods.should include(:weight)
+    AttributeItem.public_instance_methods.should include(:weight=)
+    AttributeItem.montgomery_attrs.should include(:weight)
   end
 end
--- spec/entity_spec.rb
+++ spec/entity_spec.rb
@@ -6,25 +6,7 @@ end
 
 describe 'Montgomery::Entity' do
   it 'should include Attribute and Id modules to base' do
-    EntityItem.included_modules.should.include(Montgomery::Entity::Attribute)
-    EntityItem.included_modules.should.include(Montgomery::Entity::Id)
+    EntityItem.included_modules.should include(Montgomery::Entity::Attribute)
+    EntityItem.included_modules.should include(Montgomery::Entity::Id)
   end
-
-=begin
-
-  describe 'included into an Item' do
-    before do
-      @doc = {
-        '_id' => Factory.mongo_object_id,
-        'name' => Faker::Lorem.sentence,
-        'weight' => rand(100)
-      }
-      @item = Item.new(@doc)
-    end
-
-    it 'should return _id' do
-      @item._id.should.equal(@doc['_id'])
-    end
-  end
-=end
 end
